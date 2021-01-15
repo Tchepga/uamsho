9c2c707d1a90a4bbb415aab155e9bf397c99b27f
Tchepga <patrick.tchepga-kouamedjo@utbm.fr>
2021-01-16 00:12:26 +0100

    deploy config

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: gunicorn njietcheu_livre.wsgi
+
+ heroku config:set DISABLE_COLLECTSTATIC=1
--- njietcheu_livre/settings.py
+++ njietcheu_livre/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django-heroku
+# import django_heroku
 
 # Activate Django-Heroku.
-django-heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
