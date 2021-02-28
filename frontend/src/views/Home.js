0772f12a40c6d95395c7c81b7608605e45138c1b
Tchepga <azera9730@gmail.com>
2021-02-28 21:08:28 +0100

    fake commit

--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -7,11 +7,20 @@ import OnTop from '../components/ontop/OnTop'
 export default class Home extends Component {
     render() {
         return (
-            <Fragment>
+            <div id="home">
                 <Menu />
                 <Carousel />
                 <OnTop />
-            </Fragment>
+                <div id="services" className="row" >
+                    <div className="col-3 text-center">
+                        <div className="mt-5">
+                            <i className="fas fa-truck fa-2x mt-4"></i>
+                        </div>
+                    </div>
+                    <i className="col-3 fas fa-exchange-alt fa-2x text-center mt-5"></i>
+
+                </div>
+            </div>
         )
     }
 }
